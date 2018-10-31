b17da7880d544f2d1ddcfcfc40f5201274f54f39
Matthias Nemeth <manedev79@gmail.com>
2018-10-31 23:24:22 +0100

    Add i-test for Timesheet application

--- src/test/java/com/github/manedev79/timesheet/TimesheetApplicationTests.java
+++ src/test/java/com/github/manedev79/timesheet/TimesheetApplicationTests.java
@@ -1,16 +1,67 @@
 package com.github.manedev79.timesheet;
 
+import com.github.manedev79.timesheet.workingday.TimesheetController;
+import com.github.manedev79.timesheet.workingday.WorkingDayController;
+import com.github.manedev79.timesheet.workingday.WorkingDayDto;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.YearMonth;
+
+import static java.util.Collections.emptyList;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class TimesheetApplicationTests {
 
+    private static final YearMonth HACKTOBER = YearMonth.of(2018, 10);
+    private static final LocalDate HACKTOBER_LAST_DAY = LocalDate.of(HACKTOBER.getYear(), HACKTOBER.getMonth(), 31);
+    private static final Instant HACKTOBER_LAST_DAY_MIDNIGHT = Instant.ofEpochSecond(HACKTOBER_LAST_DAY.toEpochDay() * 24 * 60 * 60);
+
+    @Autowired
+    private WorkingDayController workingDayController;
+
+    @Autowired
+    private TimesheetController timesheetController;
+
     @Test
     public void contextLoads() {
     }
 
+    @Test
+    public void addWorkingDay() {
+        WorkingDayDto workingDay = createWorkingDay();
+
+        WorkingDayDto addedWorkingDay = workingDayController.addWorkingDay(workingDay);
+
+        assertThat(addedWorkingDay).isEqualToIgnoringGivenFields(workingDay, "id");
+    }
+
+    @Test
+    public void roundtripForWorkingDay() {
+        WorkingDayDto addedWorkingDay = workingDayController.addWorkingDay(createWorkingDay());
+
+        assertThat(timesheetController.getTimesheetForYearMonth(HACKTOBER))
+                .contains(addedWorkingDay);
+    }
+
+    @Test
+    public void roundTripWithDate() {
+        WorkingDayDto addedWorkingDay = workingDayController.addWorkingDay(createWorkingDay());
+
+        assertThat(timesheetController.getTimesheetForMonthByDate(HACKTOBER_LAST_DAY))
+                .contains(addedWorkingDay);
+    }
+
+    private WorkingDayDto createWorkingDay() {
+        Instant end = HACKTOBER_LAST_DAY_MIDNIGHT.plus(Duration.ofHours(8));
+        return new WorkingDayDto(null, HACKTOBER_LAST_DAY, HACKTOBER_LAST_DAY_MIDNIGHT, end, "Test day", emptyList());
+    }
 }
